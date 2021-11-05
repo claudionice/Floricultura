e177630d4ffadc892a7e383b955d1b770df76c89
Claudionice Rosah <rosahclaudionice@gmail.com>
2021-11-05 15:13:47 -0300

    Cadastrando urls

--- setup/urls.py
+++ setup/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from floricultura.views import plantas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ('plantas/', plantas)
 ]
