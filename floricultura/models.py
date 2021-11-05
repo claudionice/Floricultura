6feb97ddd4f3bce853dd50c9ae005a68e64df7ab
Claudionice Rosah <rosahclaudionice@gmail.com>
2021-11-05 15:30:39 -0300

    Criando modelos plantas, vasos

--- floricultura/models.py
+++ floricultura/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+class plantas (models.Model):
+    especie = models.CharField (max_length = 30)
+    nome_cientifico = models.CharField (max_length= 30)
+    ESTACAO = (
+        ('Primavera'),
+        ('Verão'),
+        ('Outono'),
+        ('Inverno'),
+    ) 
+    estacao= models.CharField (max_length=1, choices= ESTACAO, blank=False, null=False)
+    
+    def __str__ (self):
+        return self.especie
+
+
+class vaso (models.Model):
+    material = models.CharField (max_length = 30)
+    TAMANHO = (
+        ('P', 'Pequeno'),
+        ('M', 'Medio'),
+        ('G', 'Grande'),
+    ) 
+    tamanho = models.CharField (max_length=1, choices=TAMANHO, blank=False, null=False)
+
+    def __str__ (self):
+        return self.tamanho 
+
 # Create your models here.
